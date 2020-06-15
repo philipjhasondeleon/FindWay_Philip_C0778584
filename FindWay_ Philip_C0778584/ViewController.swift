411c0e6392acf0bb9d9a9ba5b7e82a7743cce5ec
Philip <jhasondeleon@gmail.com>
2020-06-15 17:42:01 -0400

    final commit

--- FindWay_ Philip_C0778584/ViewController.swift
+++ FindWay_ Philip_C0778584/ViewController.swift
@@ -14,26 +14,27 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     @IBOutlet weak var btnFindWay: UIButton!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var segmentType: UISegmentedControl!
-    
+
     var locationManager = CLLocationManager()
-    
     var aLat: CLLocationDegrees??
     var aLon: CLLocationDegrees??
     var location: CLLocation?
     
-  
+    @IBAction func indexChanged(_ sender: Any) {
+           yourWay()
+    }
+     @IBAction func findMyWay(_ sender: Any) {
+            yourWay()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.delegate = self
         locationManager.delegate = self
-       
         locationManager.requestWhenInUseAuthorization()
-        
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = kCLDistanceFilterNone
-        
         locationManager.startUpdatingLocation()
         
         mapView.showsUserLocation = true
@@ -54,7 +55,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
-        renderer.strokeColor = UIColor.yellow
+        renderer.strokeColor = UIColor.black
         renderer.lineWidth = 3;
         return renderer
     }
@@ -67,7 +68,45 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
     }
     
- 
+    func yourWay(){
+        self.mapView.removeOverlays(self.mapView.overlays)
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!), addressDictionary: nil))
+        
+        if(aLat == nil || aLon == nil)
+        {
+                let alertController = UIAlertController(title: "Error", message:
+                "Please select your destination!!", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: .default))
+
+                self.present(alertController, animated: true, completion: nil)
+        }
+        else
+        {
+            request.destination = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: aLat as! CLLocationDegrees, longitude: aLon as! CLLocationDegrees), addressDictionary: nil))
+            request.requestsAlternateRoutes = false
+        }
+       
+        switch segmentType.selectedSegmentIndex
+        {
+            case 0:
+                request.transportType = .walking
+            case 1:
+                request.transportType = .automobile
+            default:
+                break
+        }
+        let directions = MKDirections(request: request)
+
+        directions.calculate { [unowned self] response, error in
+            guard let unwrappedResponse = response else { return }
+
+            for route in unwrappedResponse.routes {
+                self.mapView.addOverlay(route.polyline)
+                self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+            }
+        }
+    }
     
     
      func addAnnotation(location: CLLocationCoordinate2D)
